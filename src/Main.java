43963c2daaf68ac515e4874a5679dfcb3fb0d766
Svyatoslav Starkov <svyatoslav.starkov@yandex.ru>
2025-02-26 21:52:50 +0500

    Сhanges main

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import java.sql.SQLOutput;
 public class Main {
 
     public static void main(String[] args) {
-        int ticket = 24360;
-        int milesRubles = 20;
+        int ticket = 24365; // Стоимость билета
+        int milesRubles = 20; // Рублей за 1 милю
         int bonusMiles = ticket / milesRubles;
 
-        System.out.println("Количество бонустных миль - " + bonusMiles);
+        System.out.println("За билет стоимостью " + ticket + " рублей будет начислено " + bonusMiles + " миль.");
     }
 
 }
